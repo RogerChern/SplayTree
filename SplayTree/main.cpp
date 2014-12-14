8d69a0268f5c86605d23647695e45a128efdeff4
RogerChern <chenyuntao08@gmail.com>
2014-12-14 21:34:11 +0800

    a randomized test of insert( ) / remove( )

--- SplayTree/main.cpp
+++ SplayTree/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+#include "SplayTree.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    SplayTree st;
+    vector<int> test1;
+    vector<int> test2;
+
+    for (int i = 0; i < 1000000; ++i) {
+        test1.push_back(i);
+        test2.push_back(i);
+    }
+    shuffle(test1.begin(), test1.end(), mt19937_64(clock()));
+    shuffle(test2.begin(), test2.end(), mt19937_64(clock()));
+    
+    auto begin = clock();
+    for (int i : test1) {
+        st.insert(i, i);
+    }
+    
+    for (int i : test2) {
+        st.remove(i);
+    }
+    auto end = clock();
+    
+    cout << double(end - begin) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
     return 0;
 }
