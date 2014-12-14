a2d521b3ed6fffcee1afed6a0434c1ccd00aebfe
RogerChern <chenyuntao08@gmail.com>
2014-12-14 19:40:36 +0800

    Initial Commit

---
+++ SplayTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SplayTree
+//
+//  Created by RogerChen on 14/12/14.
+//  Copyright (c) 2014年 RogerChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
